a38bdba8a1694e6223adc61de176e7a02412d948
Maycon Augusto <4804325+mayaugusto7@users.noreply.github.com>
2021-10-19 12:28:40 -0300

    Configure CORS

--- src/main/java/br/com/lovecode/ecommerce/repository/ProductCategoryRepository.java
+++ src/main/java/br/com/lovecode/ecommerce/repository/ProductCategoryRepository.java
@@ -3,6 +3,8 @@ package br.com.lovecode.ecommerce.repository;
 import br.com.lovecode.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {}
--- src/main/java/br/com/lovecode/ecommerce/repository/ProductRepository.java
+++ src/main/java/br/com/lovecode/ecommerce/repository/ProductRepository.java
@@ -2,5 +2,7 @@ package br.com.lovecode.ecommerce.repository;
 
 import br.com.lovecode.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {}
