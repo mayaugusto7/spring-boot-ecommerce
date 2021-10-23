40bd2545698537ffad488ffc9ba5df18b60f1a23
Maycon Augusto <4804325+mayaugusto7@users.noreply.github.com>
2021-10-22 21:44:34 -0300

    Find Page Product and Pageable products by category

--- src/main/java/br/com/lovecode/ecommerce/repository/ProductRepository.java
+++ src/main/java/br/com/lovecode/ecommerce/repository/ProductRepository.java
@@ -1,8 +1,14 @@
 package br.com.lovecode.ecommerce.repository;
 
 import br.com.lovecode.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+}
